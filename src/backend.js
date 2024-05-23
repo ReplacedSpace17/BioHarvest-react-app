12b21020f5f4ef43b138b196ecca8b93a766c9bc
ReplacedSpace17 <replacedspace17@gmail.com>
2024-05-23 02:18:23 -0600

    Update page title and backend URL
    ```

--- src/backend.js
+++ src/backend.js
@@ -1,6 +1,6 @@
 // config.js
 
-const backendUrl = 'http://20.102.109.114:3002'; // Reemplaza con la URL real de tu backend
-//const backendUrl = 'http://localhost:3002/'; // Reemplaza con la URL real de tu backend
+//const backendUrl = 'http://20.102.109.114:3002'; // Reemplaza con la URL real de tu backend
+const backendUrl = 'http://localhost:3002'; // Reemplaza con la URL real de tu backend
 
 export default backendUrl;
